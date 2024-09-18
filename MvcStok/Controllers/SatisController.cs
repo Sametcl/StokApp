9801ae27bb23f0f86f45e7789f2114e1795c7f13
Sametcl <samet2016.2022@gmail.com>
2024-09-18 15:19:50 +0300

    StokApp

---
+++ MvcStok/Controllers/SatisController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Controllers;
+using MvcStok.Models.Entity;
+namespace MvcStok.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        MvcDbstokEntities1 db = new MvcDbstokEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p)
+        {
+            db.TBLSATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
